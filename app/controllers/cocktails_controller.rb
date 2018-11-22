e51823ccad257be218e21f6c7a1a6e965b6b9822
SUMITA TAKAHASHI <sumi.5.goo@gmail.com>
2018-11-22 17:00:22 +0900

    done

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -9,7 +9,11 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    @restaurant.save
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "new"
+    end
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :cocktails do
+  root to: 'pages#home'
+  resources :cocktails, only: [:create, :index, :show, :new] do
+    resources :doses, only: [:new, :create, :destroy]
   end
+  resources :doses, only:[:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
